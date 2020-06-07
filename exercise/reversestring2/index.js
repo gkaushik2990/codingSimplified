66ec1187938765a94437a5d7c72442e9598d60e9
gkaushik <gkaushik2990@gmail.com>
2020-06-07 17:55:54 +0530

    Commiting Reverse String 2

---
+++ exercise/reversestring2/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+// Using Stack
+function reverse(str) {
+    var a = str.split("");
+    var revA = [];
+    for(var i=a.length-1;i>=0;i--){
+        revA.push(a[i]);
+    }
+    return revA.join("");
+}
+
+module.exports = reverse;
