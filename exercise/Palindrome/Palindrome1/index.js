d6628f087edf04e2ee8d620450cc1a471cd40a95
gkaushik <gkaushik2990@gmail.com>
2020-06-07 22:03:35 +0530

    Commiting the Palindrome

---
+++ exercise/Palindrome/Palindrome1/index.js
@@ -0,0 +1,19 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+    //Using reduce
+    let revStr = str.split('').reduce((rev,char)=>{
+        return char+rev;
+    },"");
+
+    return revStr === str;
+}
+
+module.exports = palindrome;
---
+++ exercise/Palindrome/Palindrome3/index.js
@@ -0,0 +1,22 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+    //Using Stack
+    let revStr =[];
+    let popArr = str.split("");
+    let i = 0;
+    for(let s of str){
+        revStr[i] = popArr.pop()
+        i++;
+    }
+
+    return revStr.join('') == str;
+}
+module.exports = palindrome;
