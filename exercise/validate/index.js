f9edb1d3e600db3b9069926fd16a3d98deda2764
gkaushik <gkaushik2990@gmail.com>
2020-06-16 00:38:38 +0530

    Validate BST

---
+++ exercise/validate/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a node, validate the binary search tree,
+// ensuring that every node's left hand child is
+// less than the parent node's value, and that
+// every node's right hand child is greater than
+// the parent
+
+function validate(node, min = null, max = null) {
+    if(max){
+        if(node.data>max){
+            return false;
+        }
+    }
+    if(min){
+        if(node.data < min){
+            return false;
+        }
+    }
+    if(node){
+        if(node.left){
+            return validate(node.left,min,node.data);
+        }
+        if(node.right){
+            return validate(node.right,node.data, max);
+        }
+    }
+    return true;
+
+}
+
+module.exports = validate;
