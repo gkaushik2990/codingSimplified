d4d05f9539a2ace07749c9cd7ba45260fdc9e925
gkaushik <gkaushik2990@gmail.com>
2020-06-15 23:32:55 +0530

    Till BFS

--- exercise/levelwidth/index.js
+++ exercise/levelwidth/index.js
@@ -11,6 +11,27 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let arr = [root,'S'];
+    let sizeArr =[];
+    let count = 0;
+    while(arr.length>0){
+        let node = arr.shift();
+        
+            if(node == 'S'){
+                if(arr.length == 0){
+                    sizeArr.push(count);    
+                }else{
+                    arr.push('S');
+                    sizeArr.push(count);
+                    count= 0;
+                }
+            }else{
+                count++;
+                arr.push(...node.children);          
+            }
+        }
+    return sizeArr;
+}
 
 module.exports = levelWidth;
